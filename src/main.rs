60b333e034c146611481d44dca16f1be379d3985
Piotr <stef.piotr@wp.pl>
2021-08-25 16:55:06 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,150 @@
+extern crate sdl2;
+extern crate num_traits;
+extern crate palette;
+
+use sdl2::{
+    event::Event,
+    keyboard::Keycode,
+    pixels::Color,
+    rect::Point,
+    render::Canvas,
+    video::Window,
+};
+
+use num_traits::Num;
+
+use palette::{
+    Hsv,
+    RgbHue,
+    rgb::Rgb,
+    FromColor,
+    Pixel,
+};
+
+fn map<T: Num + Copy>(val: T, a_min: T, a_max: T, b_min: T, b_max: T) -> T {
+    // Y = (X-A)/(B-A) * (D-C) + C
+    (val-a_min)/(a_max-a_min) * (b_max-b_min) + b_min
+}
+
+const WIDTH:  u32 = 500;
+const HEIGHT: u32 = 500;
+
+fn draw_set(canvas: &mut Canvas<Window>, range: &(f32,f32), pos: &(f32,f32), max_iter: u32) -> Result<(), String> {
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            let mut a: f32;
+            let mut b: f32;
+            a = map::<f32>(x as f32, 0., WIDTH  as f32, range.0, range.1) + pos.0;
+            b = map::<f32>(y as f32, 0., HEIGHT as f32, range.0, range.1) + pos.1;
+
+            let ca = a;
+            let cb = b;
+
+            let mut iter = max_iter;
+            for i in 0..max_iter {
+                //println!("Iter: {}", i);
+                let aa = (a*a) - (b*b);
+                let bb = 2.*a*b;
+                a = aa+ca;
+                b = bb+cb;
+                if a+b > 16. {
+                    iter = i;
+                    break;
+                }
+            }
+
+            // TODO - optimalize
+            let hue          = map::<f32>(iter as f32, 0., max_iter as f32, 0., 359.);
+            let value        = if iter == max_iter {0.} else {1.};
+            let rgb: [u8; 3] = Rgb::from_color(Hsv::new(RgbHue::from_degrees(hue), 1., value)).into_format().into_raw();
+
+            //println!("{} {} {}", rgb[0], rgb[1], rgb[2]);
+            canvas.set_draw_color(Color::RGB(rgb[0], rgb[1], rgb[2]));
+            canvas.draw_point(Point::new(x as i32,y as i32))?;
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    println!("Hello, world!");
+    
+    let sdl_context = sdl2::init()?;
+    let vid_subsys = sdl_context.video()?;
+
+    let window = vid_subsys
+        .window("Mandelbrot Set", WIDTH, HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut draw = true;
+
+    let mut max_iter: u32 = 100;
+
+    //-0.715181 -i0.230028
+    //let mut range: (f32,f32) = (-0.715181,-0.230028);
+    let mut range: (f32,f32) = (-2.5,1.5);
+    let mut pos:   (f32,f32) = (0.,0.);
+
+    let mut zoom:      f32 = 1.;
+    let mut mov_speed: f32 = 0.4;
+
+    let zoom_factor:      f32 = 0.4;
+    let mov_speed_factor: f32 = 0.925;
+
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'main,
+                Event::KeyDown {
+                    keycode: key,
+                    ..
+                } => {
+                    match key {
+                        Some(Keycode::E) => { range.0 += zoom_factor/zoom; range.1 -= zoom_factor/zoom; zoom += 1.; mov_speed *= mov_speed_factor; },
+                        Some(Keycode::Q) => { range.0 -= zoom_factor/zoom; range.1 += zoom_factor/zoom; zoom -= 1.; mov_speed /= mov_speed_factor; },
+                        Some(Keycode::W) => { pos.1 -= mov_speed; },
+                        Some(Keycode::S) => { pos.1 += mov_speed; },
+                        Some(Keycode::A) => { pos.0 -= mov_speed; },
+                        Some(Keycode::D) => { pos.0 += mov_speed; },
+                        Some(Keycode::K) => { max_iter += 10; },
+                        Some(Keycode::J) => { if max_iter != 0 { max_iter -= 10; } },
+                        _ => {}
+                    }
+
+                    draw = true
+                },
+                _ => {}
+            }
+        }
+
+        if draw {
+            println!("Range:       {} {}", range.0, range.1);
+            println!("Pos:         {} {}", pos.0, pos.1);
+            println!("Zoom/factor: {} {}", zoom, zoom_factor);
+            println!("Mov/factor : {} {}", mov_speed, mov_speed_factor);
+            println!("Max iter   : {}",    max_iter);
+            canvas.set_draw_color(Color::RGB(0, 0, 0));
+            canvas.clear();
+
+            println!("drawing");
+
+            draw_set(&mut canvas, &range, &pos, max_iter)?;
+
+            println!("frame");
+            canvas.present();
+
+            draw = false;
+        }
+
+        //thread::sleep(time::Duration::from_millis(100));
+    }
+
+
+    Ok(())
+}
